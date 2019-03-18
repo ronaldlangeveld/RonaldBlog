7b9ff61301cc881139c4a87b3c2ec8e7e3588599
Ronald Langeveld <ronald@Ronalds-MacBook-Pro.local>
2019-03-18 15:39:00 +0900

    added sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,6 +41,7 @@ module.exports = {
           `gatsby-transformer-remark`,
           `gatsby-plugin-sharp`,
           `gatsby-transformer-sharp`,
+          `gatsby-plugin-sitemap`
 
     ]
   }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,42 +5,42 @@ import { Link } from "gatsby"
 class Navbar extends Component {
     state = {
         isActive: false
-      };
+    };
 
-      handleClick = () => {
-          console.log('handleClick');
+    handleClick = () => {
+        console.log('handleClick');
         this.setState(state => ({ isActive: !state.isActive }));
-      };
+    };
 
 
-  render() {
-    return (
-        <div>
-            <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
-                <div className="navbar-brand">
-                    <a onClick={this.handleClick} role="button" className={this.state.isActive ? 'navbar-burger burger is-active': 'navbar-burger burger'} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                    </a>
-                </div>
+    render() {
+        return (
+            <div>
+                <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
+                    <div className="navbar-brand">
+                        <a onClick={this.handleClick} role="button" className={this.state.isActive ? 'navbar-burger burger is-active' : 'navbar-burger burger'} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                            <span aria-hidden="true"></span>
+                            <span aria-hidden="true"></span>
+                            <span aria-hidden="true"></span>
+                        </a>
+                    </div>
 
-                <div id="navbarBasicExample" className={this.state.isActive ? 'navbar-menu is-active': 'navbar-menu'}>
-                    <div className="navbar-start">
-                        <Link to="/" className="navbar-item">
-                            Home
+                    <div id="navbarBasicExample" className={this.state.isActive ? 'navbar-menu is-active' : 'navbar-menu'}>
+                        <div className="navbar-start">
+                            <Link to="/" className="navbar-item">
+                                Home
     </Link>
-                    </div>
+                        </div>
 
-                    <div className="navbar-end">
-                        <a className="navbar-item" href="https://twitter.com/ronaldlangeveld" target="_blank" rel="noopener noreferrer">Twitter</a>
-                        <a className="navbar-item" href="https://github.com/ronaldlangeveld" target="_blank" rel="noopener noreferrer">Github</a>
+                        <div className="navbar-end">
+                            <a className="navbar-item" href="https://twitter.com/ronaldlangeveld" target="_blank" rel="noopener noreferrer">Twitter</a>
+                            <a className="navbar-item" href="https://github.com/ronaldlangeveld" target="_blank" rel="noopener noreferrer">Github</a>
+                        </div>
                     </div>
-                </div>
-            </nav>
-        </div>
-    )
-  }
+                </nav>
+            </div>
+        )
+    }
 }
 
 
