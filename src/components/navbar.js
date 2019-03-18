d97723c23dc10384793a138787a4a3ed59de13d6
Ronald Langeveld <ronald@Ronalds-MacBook-Pro.local>
2019-03-18 15:16:43 +0900

    added burger menu

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,37 +1,47 @@
-import React from 'react';
-import { Link } from "gatsby";
+import React, { Component } from 'react'
+import { Link } from "gatsby"
 
-const Navbar = () => {
+
+class Navbar extends Component {
+    state = {
+        isActive: false
+      };
+
+      handleClick = () => {
+          console.log('handleClick');
+        this.setState(state => ({ isActive: !state.isActive }));
+      };
+
+
+  render() {
     return (
         <div>
-            <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
-                <div class="navbar-brand">
-                    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
+                <div className="navbar-brand">
+                    <a onClick={this.handleClick} role="button" className={this.state.isActive ? 'navbar-burger burger is-active': 'navbar-burger burger'} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
                         <span aria-hidden="true"></span>
                         <span aria-hidden="true"></span>
                         <span aria-hidden="true"></span>
                     </a>
                 </div>
 
-                <div id="navbarBasicExample" class="navbar-menu">
-                    <div class="navbar-start">
-                        <Link to="/" class="navbar-item">
+                <div id="navbarBasicExample" className={this.state.isActive ? 'navbar-menu is-active': 'navbar-menu'}>
+                    <div className="navbar-start">
+                        <Link to="/" className="navbar-item">
                             Home
     </Link>
                     </div>
 
-                    <div class="navbar-end">
+                    <div className="navbar-end">
                         <a className="navbar-item" href="https://twitter.com/ronaldlangeveld" target="_blank" rel="noopener noreferrer">Twitter</a>
                         <a className="navbar-item" href="https://github.com/ronaldlangeveld" target="_blank" rel="noopener noreferrer">Github</a>
                     </div>
                 </div>
             </nav>
         </div>
-    );
+    )
+  }
 }
 
+
 export default Navbar;
-
-
-
-
